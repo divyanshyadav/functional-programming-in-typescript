2e2d733077c09304a3c6da771ef3b23e7e0bd7e7
Divyansh <divyansh.yadav@gmail.com>
2022-06-18 21:48:11 +0100

    cleanup

--- src/functor/Maybe.test.ts
+++ src/functor/Maybe.test.ts
@@ -6,36 +6,36 @@ import Maybe from './Maybe'
 
 describe('Maybe', () => {
     it('should return the same container if value is null', () => {
-        const container = new Maybe(null)
+        const container: Maybe = new Maybe(null)
         expect(container.map(a => a)).toBe(container)
     })
 
     it('should return the same container if value is null', () => {
-        const container = new Maybe(undefined)
+        const container: Maybe = new Maybe(undefined)
         expect(container.map(a => a)).toBe(container)
     })
 
     it('should return new container if value is not null or undefined', () => {
-        const container = new Maybe(1)
+        const container: Maybe = new Maybe(1)
         expect(container.map(a => a)).not.toBe(container)
         expect(container.map(a => a) instanceof Maybe).toBe(true)
     })
 
     it('should return true if value is matched', () => {
-        const container = new Maybe('hello')
+        const container: Maybe = new Maybe('hello')
         expect(container.map(match(/h/g)).value).toEqual(['h'])
     })
 
     it("should return empty container if value doesn't matched", () => {
-        const container = new Maybe('hello')
-        const emptyContainer = container.map(match(/a/g))
+        const container: Maybe = new Maybe('hello')
+        const emptyContainer: Maybe = container.map(match(/a/g))
         expect(emptyContainer.isNothing()).toEqual(true)
         expect(emptyContainer.map(a => a)).toEqual(emptyContainer)
     })
 
     it('should not return empty container if prop on object', () => {
-        const container = new Maybe({ name: 'red', age: 10 })
-        const newContainer = container
+        const container: Maybe = new Maybe({ name: 'red', age: 10 })
+        const newContainer: Maybe = container
             .map(prop('age'))
             .map(add(10))
             .map(add(10))
@@ -45,7 +45,10 @@ describe('Maybe', () => {
 
     it('should return empty container if prop not on object', () => {
         const user = { name: 'red' }
-        const getAgeAndAddTen = compose(map(add(10)), safeProp('age'))
+        const getAgeAndAddTen: (user: object) => Maybe = compose(
+            map(add(10)),
+            safeProp('age')
+        )
         expect(getAgeAndAddTen(user).isNothing()).toBeTruthy()
     })
 })
